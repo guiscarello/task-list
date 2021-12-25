a440fbb30a4e5808a2885eeed3d76bccef742f98
Guillermo <guillermoscarello@gmail.com>
2021-12-25 13:05:10 -0300

    added mock tasks

---
+++ src/app/mock-stacks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+	{
+		id: 1,
+		text: "Terminar primer modulo de Angular",
+		date: "Agosto 5 a las 22:00",
+		reminder: true
+	},
+	{
+		id: 2,
+		text: "Investigar sobre Boostrap",
+		date: "Agosto 8 a las 20:00",
+		reminder: true
+	},
+	{
+		id: 3,
+		text: "Investigar sobre Three JS",
+		date: "Septiembre 6 a las 17:00",
+		reminder: false
+	},
+	{
+		id: 4,
+		text: "Ivestigar sobre Blender",
+		date: "Agosto 18 a las 02:00",
+		reminder: false
+	}
+]
